d16d555f43382d74e5d64088eb50fadeb13aa79d
sajid <sajidbrong@gmail.com>
2025-01-08 13:13:13 -0500

    update the github action for create page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
 // src/App.js
+
+/**
+ * The main entry point of the React application.
+ * This component includes the Navbar, Sidebar, and main Content.
+ * 
+ * @file App.js
+ * @module App
+ * @requires React
+ * @requires ./App.css
+ * @requires ./components/Content
+ * @requires ./components/Navbar
+ * @requires ./components/Sidebar
+ */
+
 import React from 'react';
 
 import './App.css';
@@ -6,6 +20,12 @@ import Content from './components/Content.jsx';
 import Navbar from './components/Navbar.jsx';
 import Sidebar from './components/Sidebar.jsx';
 
+/**
+ * Renders the main application structure, including the Navbar, Sidebar, and Content.
+ * 
+ * @component
+ * @returns {JSX.Element} The rendered App component.
+ */
 function App() {
   return (
     <div className="app">
